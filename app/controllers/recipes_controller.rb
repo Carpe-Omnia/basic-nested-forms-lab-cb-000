627ca8076264ef232781294c628e675643076f70
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-04-05 03:12:09 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -20,6 +20,6 @@ class RecipesController < ApplicationController
 
   private
   def recipe_params
-    params.require(:recipe).permit(:title, ingredients: [:id, :name, :quantity])
+    params.require(:recipe).permit(:title, ingredients: [ :name, :id, :quantity])
   end
 end
